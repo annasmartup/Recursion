795485af6830e78bed664c658a7f4a4ab950c82a
Anna_skazka <smartupfit@yandex.ru>
2024-03-08 11:47:37 +1000

    Выполнено 2 задание

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if ((n == 0) && (m != 0))
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int m = 3;
+        int n = 4;
+        int result = Ackermann(m, n);
+        Console.WriteLine(result);
+    }
+}
