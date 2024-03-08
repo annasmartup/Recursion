9889f163deef70c2625ac5e0b8e54eccd7d5b20c
Anna_skazka <smartupfit@yandex.ru>
2024-03-08 15:35:49 +1000

    Сделана 3 задача

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+
+    static void ReverseArray(int[] array)
+    {
+        if (array.Length == 0)
+        {
+            return;
+
+        }
+        ReverseArray(array.Skip(1).ToArray());
+        Console.Write(array[0] + " ");
+    }
+
+    static void Main(string[] args)
+    {
+        int [] array = {1, 2, 5, 0, 10, 34};
+        ReverseArray(array);
+    }
+}
