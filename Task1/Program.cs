6f5c31a403272d240c55bf274cebcfd802e8ea48
Anna_skazka <smartupfit@yandex.ru>
2024-03-08 11:08:53 +1000

    Сделана 1 задача

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+
+    static void GetDigitSequence(int m, int n)
+    {
+        if (m > n)
+        {
+            return;
+        }
+        GetDigitSequence(m, n - 1);
+        Console.Write(n + ",");
+
+    }
+    static void Main(string[] args)
+    {
+        int M = 5;
+        int N = 25;
+        GetDigitSequence(M, N);
+    }
+}
